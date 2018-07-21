63a757df33c78cf833c4040571a34649ff0f4158
Ankit Sharma <ankit.sharma25@gmail.com>
2018-07-21 22:07:57 +0530

    Authentication added

--- server/model/user.js
+++ server/model/user.js
@@ -1,41 +1,63 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 const validator = require('validator');
-
-var User = mongoose.model('Users',{
-    name:{
-        type:String,
-        required:true,
-        trim:true,
-        minlength:1
-    },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        minlength:1,
-        unique:true,
-        validate:{
-            validator:validator.isEmail,
-            message: `{value} is not a valid email`
-        }
-    },
-    password:{
-        type:String,
-        required:true,
-        trim:true,
-        minlength:8
-    },
-    tokens:[{
-        access:{
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+var userSchema = new mongoose.Schema({
+        name:{
             type:String,
-            required:true
+            required:true,
+            trim:true,
+            minlength:1
         },
-        token:{
+        email:{
             type:String,
-            required:true
-        }
-    }]
-});
+            required:true,
+            trim:true,
+            minlength:1,
+            unique:true,
+            validate:{
+                validator:validator.isEmail,
+                message: `{value} is not a valid email`
+            }
+        },
+        password:{
+            type:String,
+            required:true,
+            trim:true,
+            minlength:8
+        },
+        tokens:[{
+            access:{
+                type:String,
+                required:true
+            },
+            token:{
+                type:String,
+                required:true
+            }
+        }]
+})
+
+userSchema.methods.toJSON = function(){
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject,['_id','email']);
+};
+
+userSchema.methods.generateAuthToken = function(){
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id:user._id.toHexString(),access},'abc123').toString();
+    // user.tokens.push({access,token});
+    user.tokens = user.tokens.concat([{access,token}]);
+
+    return user.save().then(()=>{
+        return token;
+    });
+};
+
+var User = mongoose.model('Users',userSchema);
 
 module.exports = {
     User
--- server/server.js
+++ server/server.js
@@ -89,8 +89,11 @@ app.patch('/todos/:id',(req,res)=>{
 app.post('/users',(req,res)=>{
     var body = _.pick(req.body,['name','email','password']);
     var user = new User(body);
-    user.save().then((user)=>{
-        res.send(user);
+    user.save().then(()=>{
+        // res.send(user);
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
     }).catch((e)=>{
         res.status(400).send(e);
     });
